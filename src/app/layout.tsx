e78a656189effd28b2d938f35b5484523f7a7a19
Barbaros ZÖNGÜR <140146415+barbaroszngr@users.noreply.github.com>
2024-07-09 20:55:39 +0300

    feat: theme-provider

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,12 @@
 import type { Metadata } from "next";
 import { ClerkProvider } from "@clerk/nextjs";
-import { Inter } from "next/font/google";
+//import { Inter } from "next/font/google";
+import { Plus_Jakarta_Sans } from 'next/font/google'
 import "./globals.css";
 import { Toaster } from "@/components/ui/toaster";
+import { ThemeProvider } from "@/context/them-provider";
 
-const inter = Inter({ subsets: ["latin"] });
+const jakarta = Plus_Jakarta_Sans({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -19,8 +21,15 @@ export default function RootLayout({
   return (
     <ClerkProvider>
       <html lang="en">
-        <body className={inter.className}>{children}
-        <Toaster />
+        <body className={jakarta.className}>
+        <ThemeProvider
+            attribute="class"
+            defaultTheme="light"
+            disableTransitionOnChange
+          >
+            {children}
+            <Toaster />
+          </ThemeProvider>
         </body>
       </html>
     </ClerkProvider>
