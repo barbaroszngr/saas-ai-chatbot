77a86af87eb02b89a6a71135cf8771a612a13f8d
Barbaros ZÖNGÜR <140146415+barbaroszngr@users.noreply.github.com>
2024-07-09 16:44:31 +0300

    feat: otp-form.tsx otp-index.tsx

--- src/components/forms/sign-up/otp-form.tsx
+++ src/components/forms/sign-up/otp-form.tsx
@@ -1,3 +1,4 @@
+import OTPInput from '@/components/otp'
 import React from 'react'
 
 type Props = {
@@ -9,7 +10,15 @@ const OTPForm = ({ onOTP, setOTP }: Props) => {
   return (
     <>
       <h2 className="text-gravel md:text-4xl font-bold">Enter OTP</h2>
-      
+      <p className="text-iridium md:text-sm">
+        Enter the one time password that was sent to your email.
+      </p>
+      <div className="w-full justify-center flex py-5">
+        <OTPInput
+          otp={onOTP}
+          setOtp={setOTP}
+        />
+      </div>
     </>
   )
 }
