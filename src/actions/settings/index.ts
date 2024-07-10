7fa294c3277dce86e2fb842e8cd667be0a4523d8
Barbaros ZÖNGÜR <140146415+barbaroszngr@users.noreply.github.com>
2024-07-10 20:22:27 +0300

    feat: onGetAllAccountDomains added

--- src/actions/settings/index.ts
+++ src/actions/settings/index.ts
@@ -26,3 +26,38 @@ export const onGetSubscriptionPlan = async () => {
         
     }
 }
+
+export const onGetAllAccountDomains = async () => {
+    const user = await currentUser()
+    if (!user) return
+    try {
+      const domains = await client.user.findUnique({
+        where: {
+          clerkId: user.id,
+        },
+        select: {
+          id: true,
+          domains: {
+            select: {
+              name: true,
+              icon: true,
+              id: true,
+              customer: {
+                select: {
+                  chatRoom: {
+                    select: {
+                      id: true,
+                      live: true,
+                    },
+                  },
+                },
+              },
+            },
+          },
+        },
+      })
+      return { ...domains }
+    } catch (error) {
+      console.log(error)
+    }
+  }
