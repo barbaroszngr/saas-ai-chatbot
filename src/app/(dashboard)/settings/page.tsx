6e687a91957dcd52705a50330198aaa9663a0807
Barbaros ZÖNGÜR <140146415+barbaroszngr@users.noreply.github.com>
2024-07-12 15:31:35 +0300

    feat: updated

--- src/app/(dashboard)/settings/page.tsx
+++ src/app/(dashboard)/settings/page.tsx
@@ -1,5 +1,7 @@
 import Infobar from '@/components/infobar'
 import BillingSettings from '@/components/settings/billing-settings'
+import ChangePassword from '@/components/settings/change-password'
+import DarkModetoggle from '@/components/settings/dark-mode'
 import React from 'react'
 
 type Props = {}
@@ -10,6 +12,8 @@ const page = (props: Props) => {
     <Infobar />
     <div className="overflow-y-auto w-full chat-window flex-1 h-0 flex flex-col gap-10">
         <BillingSettings />
+        <DarkModetoggle />
+        <ChangePassword />
     </div>
     </>
   )
