8fcdfe493077b08ff11c6ac10beef1da6a04d6f2
Barbaros ZÖNGÜR <140146415+barbaroszngr@users.noreply.github.com>
2024-07-12 15:32:43 +0300

    feat: billing-settings modified

--- src/components/settings/billing-settings.tsx
+++ src/components/settings/billing-settings.tsx
@@ -1,12 +1,21 @@
 import { onGetSubscriptionPlan } from '@/actions/settings'
 import React from 'react'
 import Section from '../section-label'
+import { Card, CardContent, CardDescription } from '../ui/card'
+import { Plus } from 'lucide-react'
+import { pricingCards } from '@/constants/landing-page'
 
 type Props = {}
 
 const BillingSettings = async (props: Props) => {
-    const plan = await onGetSubscriptionPlan()
-    
+  //WIP: Add stripe subscription form.
+  const plan = await onGetSubscriptionPlan()
+  const planFeatures = pricingCards.find(
+    (card) => card.title.toUpperCase() === plan?.toUpperCase()
+  )?.features
+  if (!planFeatures) return
+
+  console.log(planFeatures)
   return (
     <div className="grid grid-cols-1 lg:grid-cols-5 gap-10">
       <div className="lg:col-span-1">
@@ -15,7 +24,18 @@ const BillingSettings = async (props: Props) => {
           message="Add payment information, upgrade and modify your plan."
         />
       </div>
-      
+      <div className="lg:col-span-2 flex justify-start lg:justify-center ">
+       <Card className="border-dashed bg-cream border-gray-400 w-full cursor-pointer h-[270px] flex justify-center items-center">
+                <CardContent className="flex gap-2 items-center">
+                  <div className="rounded-full border-2 p-1">
+                    <Plus className="text-gray-400" />
+                  </div>
+                  <CardDescription className="font-semibold">
+                    Upgrade Plan
+                  </CardDescription>
+                </CardContent>
+              </Card>
+      </div>
     </div>
   )
 }
