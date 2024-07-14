f5bffa11acb066a7db3e24b8a7a7d6e90b3b22f0
Barbaros ZÖNGÜR <140146415+barbaroszngr@users.noreply.github.com>
2024-07-14 16:48:59 +0300

    feat:sechemas updated

--- src/schemas/auth.schema.ts
+++ src/schemas/auth.schema.ts
@@ -46,7 +46,10 @@ export type UserRegistrationProps = {
     password: string
   }
 
- 
+  export type ChangePasswordProps = {
+    password: string
+    confirmPassword: string
+  }
 
   export const UserLoginSchema: ZodType<UserLoginProps> = z.object({
     email: z.string().email({ message: 'You did not enter a valid email' }),
@@ -58,3 +61,21 @@ export type UserRegistrationProps = {
       }),
   })
   
+  export const ChangePasswordSchema: ZodType<ChangePasswordProps> = z
+  .object({
+    password: z
+      .string()
+      .min(8, { message: 'Your password must be atleast 8 characters long' })
+      .max(64, {
+        message: 'Your password can not be longer then 64 characters long',
+      })
+      .refine(
+        (value) => /^[a-zA-Z0-9_.-]*$/.test(value ?? ''),
+        'password should contain only alphabets and numbers'
+      ),
+    confirmPassword: z.string(),
+  })
+  .refine((schema) => schema.password === schema.confirmPassword, {
+    message: 'passwords do not match',
+    path: ['confirmPassword'],
+  })
