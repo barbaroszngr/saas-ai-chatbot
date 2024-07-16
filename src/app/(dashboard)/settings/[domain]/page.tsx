ef8ed51af1de9ab88e28bc09701c7f779062fd6d
Barbaros ZÖNGÜR <140146415+barbaroszngr@users.noreply.github.com>
2024-07-16 19:33:01 +0300

    updated

--- src/app/(dashboard)/settings/[domain]/page.tsx
+++ src/app/(dashboard)/settings/[domain]/page.tsx
@@ -1,4 +1,5 @@
 import { onGetCurrentDomainInfo } from '@/actions/settings'
+import BotTrainingForm from '@/components/forms/settings/bot-training'
 import SettingsForm from '@/components/forms/settings/form'
 import InfoBar from '@/components/infobar'
 import { redirect } from 'next/navigation'
@@ -20,8 +21,12 @@ const DomainSettingsPage = async ({params}: Props) => {
           id={domain.domains[0].id}
           name={domain.domains[0].name}
         />
-        
-      </div>
+         <BotTrainingForm id={domain.domains[0].id} />
+        {/*<ProductTable
+          id={domain.domains[0].id}
+          products={domain.domains[0].products || []}
+        />*/}
+      </div> 
   </>
   )
   
