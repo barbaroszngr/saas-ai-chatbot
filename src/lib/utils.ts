63fdc54de6118ccabd5fcbdfd39a908a10c8d911
Barbaros ZÖNGÜR <140146415+barbaroszngr@users.noreply.github.com>
2024-07-16 19:28:30 +0300

    feat: pusherClient, pusherServer added

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,51 @@
 import { type ClassValue, clsx } from "clsx"
 import { twMerge } from "tailwind-merge"
+import PusherClient from 'pusher-js'
+import PusherServer from 'pusher'
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
+}
+
+export const pusherServer = {}
+/*export const pusherServer = new PusherServer({
+  appId: process.env.NEXT_PUBLIC_PUSHER_APP_ID as string,
+  key: process.env.NEXT_PUBLIC_PUSHER_APP_KEY as string,
+  secret: process.env.NEXT_PUBLIC_PUSHER_APP_SECRET as string,
+  cluster: process.env.NEXT_PUBLIC_PUSHER_APP_CLUSTOR as string,
+  useTLS: true,
+})*/
+
+export const pusherClient = {}
+/*export const pusherClient = new PusherClient(
+  process.env.NEXT_PUBLIC_PUSHER_APP_KEY as string,
+  {
+    cluster: process.env.NEXT_PUBLIC_PUSHER_APP_CLUSTOR as string,
+  }
+)*/
+
+export const getMonthName = (month: number) => {
+  return month == 1
+    ? 'Jan'
+    : month == 2
+    ? 'Feb'
+    : month == 3
+    ? 'Mar'
+    : month == 4
+    ? 'Apr'
+    : month == 5
+    ? 'May'
+    : month == 6
+    ? 'Jun'
+    : month == 7
+    ? 'Jul'
+    : month == 8
+    ? 'Aug'
+    : month == 9
+    ? 'Sep'
+    : month == 10
+    ? 'Oct'
+    : month == 11
+    ? 'Nov'
+    : month == 12 && 'Dec'
 }
