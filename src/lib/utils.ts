ab1063b86a9373aedb1e13512f4767f08bf50973
Barbaros ZÖNGÜR <140146415+barbaroszngr@users.noreply.github.com>
2024-07-17 18:16:21 +0300

    updated

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -7,6 +7,12 @@ export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
 
+export const extractUUIDFromString = (url: string) => {
+  return url.match(
+    /^[0-9a-f]{8}-?[0-9a-f]{4}-?[1-5][0-9a-f]{3}-?[89ab][0-9a-f]{3}-?[0-9a-f]{12}$/i
+  )
+}
+
 export const pusherServer = {}
 /*export const pusherServer = new PusherServer({
   appId: process.env.NEXT_PUBLIC_PUSHER_APP_ID as string,
@@ -23,6 +29,19 @@ export const pusherClient = {}
     cluster: process.env.NEXT_PUBLIC_PUSHER_APP_CLUSTOR as string,
   }
 )*/
+
+export const postToParent = (message: string) => {
+  window.parent.postMessage(message, '*')
+}
+
+export const extractURLfromString = (url: string) => {
+  return url.match(/https?:\/\/[^\s"<>]+/)
+}
+
+export const extractEmailsFromString = (text: string) => {
+  return text.match(/([a-zA-Z0-9._-]+@[a-zA-Z0-9._-]+\.[a-zA-Z0-9_-]+)/gi)
+}
+
 
 export const getMonthName = (month: number) => {
   return month == 1
