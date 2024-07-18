2bc57550bc4c830f908d39ac5973cbac55ad3105
Barbaros ZÖNGÜR <140146415+barbaroszngr@users.noreply.github.com>
2024-07-18 14:22:54 +0300

    resolve merge conflict

--- src/components/chatbot/real-time.tsx
+++ src/components/chatbot/real-time.tsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import { Card } from '../ui/card'
-<<<<<<< HEAD
 //import { useRealTime } from '@/hooks/chatbot/use-chatbot'
 =======
 import { useRealTime } from '@/hooks/chatbot/use-chatbot'
@@ -22,9 +21,6 @@ type Props = {
 const RealTimeMode = ({ chatRoomId, setChats }: Props) => {
 <<<<<<< HEAD
  //useRealTime(chatRoomId, setChats)
-=======
- useRealTime(chatRoomId, setChats)
->>>>>>> 9cbbaae1e0bd8257b83692b582df024b5de2b306
 
   return (
     <Card className="px-3 rounded-full py-1 bg-orange font-bold text-white text-sm">
