3272c6e244490b9be5fb290054849b5f10b9be0d
Barbaros ZÖNGÜR <140146415+barbaroszngr@users.noreply.github.com>
2024-07-19 23:11:49 +0300

    feat: payment-form.tsx added and billing-settings updated

--- src/components/settings/billing-settings.tsx
+++ src/components/settings/billing-settings.tsx
@@ -2,20 +2,22 @@ import { onGetSubscriptionPlan } from '@/actions/settings'
 import React from 'react'
 import Section from '../section-label'
 import { Card, CardContent, CardDescription } from '../ui/card'
-import { Plus } from 'lucide-react'
+import { Check, CheckCircle2, Plus } from 'lucide-react'
 import { pricingCards } from '@/constants/landing-page'
+import Modal from '../mondal'
+import SubscriptionForm from '../forms/settings/subscription-form'
+import Image from 'next/image'
 
 type Props = {}
 
 const BillingSettings = async (props: Props) => {
-  //WIP: Add stripe subscription form.
   const plan = await onGetSubscriptionPlan()
   const planFeatures = pricingCards.find(
     (card) => card.title.toUpperCase() === plan?.toUpperCase()
   )?.features
   if (!planFeatures) return
 
-  //console.log(planFeatures)
+  console.log(planFeatures)
   return (
     <div className="grid grid-cols-1 lg:grid-cols-5 gap-10">
       <div className="lg:col-span-1">
@@ -25,7 +27,12 @@ const BillingSettings = async (props: Props) => {
         />
       </div>
       <div className="lg:col-span-2 flex justify-start lg:justify-center ">
-       <Card className="border-dashed bg-cream border-gray-400 w-full cursor-pointer h-[270px] flex justify-center items-center">
+        <Modal
+          title="Choose A Plan"
+          description="Tell us about yourself! What do you do? Let’s tailor your experience so it best suits you."
+          trigger={
+            plan && plan === 'STANDARD' ? (
+              <Card className="border-dashed bg-cream border-gray-400 w-full cursor-pointer h-[270px] flex justify-center items-center">
                 <CardContent className="flex gap-2 items-center">
                   <div className="rounded-full border-2 p-1">
                     <Plus className="text-gray-400" />
@@ -35,6 +42,33 @@ const BillingSettings = async (props: Props) => {
                   </CardDescription>
                 </CardContent>
               </Card>
+            ) : (
+              <Image
+                src="/images/creditcard.png"
+                width={400}
+                height={400}
+                alt="image"
+              />
+            )
+          }
+        >
+          <SubscriptionForm plan={plan!} />
+        </Modal>
+      </div>
+      <div className="lg:col-span-2">
+        <h3 className="text-xl font-semibold mb-2">Current Plan</h3>
+        <p className="text-sm font-semibold">{plan}</p>
+        <div className="flex gap-2 flex-col mt-2">
+          {planFeatures.map((feature) => (
+            <div
+              key={feature}
+              className="flex gap-2"
+            >
+              <CheckCircle2 className="text-muted-foreground" />
+              <p className="text-muted-foreground">{feature}</p>
+            </div>
+          ))}
+        </div>
       </div>
     </div>
   )
